be77c14ce6400937c2bbce5dc19afa1a969eb029
Pushkar Garg <pushkar@appyhigh.com>
2024-08-27 13:20:24 +0530

    minor change

--- launch.py
+++ launch.py
@@ -8,7 +8,7 @@ git = launch_utils.git
 index_url = launch_utils.index_url
 dir_repos = launch_utils.dir_repos
 
-commit_hash = launch_utils.commit_hash
+# commit_hash = launch_utils.commit_hash
 git_tag = launch_utils.git_tag
 
 run = launch_utils.run
